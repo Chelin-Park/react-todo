c527d6e8c76491ea5946417f53be693b7a8ceb84
Chelin-Park <pchin0527@gmail.com>
2023-11-21 09:05:54 -0500

    chelin park lesson-1-5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = localStorage.getItem('savedTodoList');
+    return savedTodoList ? JSON.parse(savedTodoList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
